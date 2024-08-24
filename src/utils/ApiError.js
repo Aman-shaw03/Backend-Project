aedc5e2b9cfd39856c89acbfadda38b6c88bf9e4
Aman shaw <shaw08122@gmail.com>
2024-08-24 15:12:21 +0530

    .

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,6 +8,7 @@ class ApiError extends Error{
         super(message)
         this.data = null
         this.success = false
+        // we are sending success flag with false value (if something is reading for it)
         this.statusCode = statusCode
         this.message = message
         this.errors = errors
@@ -19,3 +20,9 @@ class ApiError extends Error{
         }
     }
 }
+/*
+This line(this.data = null) explicitly sets the data property of the ApiError object to null. It doesn't 
+necessarily need to bedocumented as it's a custom property introduced within the ApiError class. It's a 
+common practice to initialize properties that might be undefined or depend on specific error scenarios. Here,
+data is likely meant to hold additional data related to the API error, but it might not always be available, so it's set to null by default.
+ */
