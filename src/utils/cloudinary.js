13b6b0236a3d684ef1feeaca5aae6c03285a2b92
Aman shaw <shaw08122@gmail.com>
2024-08-25 15:17:29 +0530

    created cloudinary utils

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
+
+// upload the file with a method , if successfull unlink the file from fs using try-catch
+ const uploadOnCloudinary = async (filePath) => {
+    try {
+        // uploading the file
+        if(!filePath) return ("Filepath not found ")
+        const response = await cloudinary.uploader.upload(filePath,{
+            resource_type: "auto"
+        })
+        //file uploaded
+        console.log("file is uploaded on cloudinary " , response.url);
+        console.log(response);
+        // since file uploaded , unlink file from fs
+        fs.unlinkSync(filePath)
+        return response
+    } catch (error) {
+        // remove the file from fs , even if file upload failed for security purposes
+        fs.unlinkSync(filePath)
+        return null
+    }
+ }
+
+ export {uploadOnCloudinary}
