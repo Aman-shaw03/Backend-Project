afa5d92a80d82bd3882c0ed982c10625cf3d9339
Aman shaw <shaw08122@gmail.com>
2024-08-26 14:18:32 +0530

    config multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer"
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "/public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ storage })
+// we use multer to handle uploading files as a middlewares for those routes which has upload file capabilities
+// with multer we choose to store file in disk so (diskStorage), it gives 2 options to create destination, and filename
+// cb is just a callback , name could be anything , but first param is to handle errors so we give "null"
