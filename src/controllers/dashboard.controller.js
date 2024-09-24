5aa183421007e65c13cd00b203c4e3c530b5f4d3
Aman shaw <shaw08122@gmail.com>
2024-09-24 15:21:42 +0530

    .

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -9,10 +9,17 @@ import {Video} from "../models/video.model.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+
+    // basically what i am gonna do is take the _id from user and find the User from MongoDB
+    //then uses aggregate pipelines for that user to get the data and return a response 
+
+    
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    // same get the User from _id and from video model get the videofiles and all through pipeline and return a response
 })
 
 export {
