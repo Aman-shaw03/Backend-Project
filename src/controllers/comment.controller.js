af560788c1d30d228e37e5ce2830d9b24ba7910f
Aman shaw <shaw08122@gmail.com>
2024-09-30 15:10:13 +0530

    doing comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,13 +1,83 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {Comment} from "../models/comment.model.js"
+import {Video} from "../models/video.model.js"
+import {Like} from "../models/like.model.js"
 
 const getVideoComments = asyncHandler( async(req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page=1, limit = 10} = req.query
+
+    const video = await Video.findById(videoId)
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(404, "invalid video id")
+    }
+
+    const options = {
+        page: parseInt(page),
+        limit: parseInt(limit)
+    }
+
+    const comment = await Comment.aggregate([
+        {
+            $match: new mongoose.Types.ObjectId(videoId)
+        },
+        {
+            $sort:{
+                $createdAt: -1
+            }
+        },
+        {
+            // get the likes
+            $lookup:{
+                from: "Like",
+                localField: "_id",
+                foreignField: "comment",
+                as: "likes",
+                pipeline: [
+                    {
+                        $match: {
+                            $liked : true
+                        }
+                    },
+                    {
+                        $group: {
+                            _id: "$liked",
+                            owners: {$push: "$likedBy"}
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            //now get the dislikes
+            $lookup:{
+                from: "Like",
+                localField: "_id",
+                foreignField: "comment",
+                as: "dislikes",
+                pipeline: [
+                    {
+                        $match: {
+                            Liked: false
+                        }
+                    },
+                    {
+                        $group:{
+                            _id: "Liked",
+                            owners: {
+                                $push: "$likedBy"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
 
 })
 
