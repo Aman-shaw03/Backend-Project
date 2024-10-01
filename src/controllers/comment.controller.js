8a0324e92b08da1820e8d4bd18947b4692e44840
Aman shaw <shaw08122@gmail.com>
2024-10-01 23:40:25 +0530

    done with all comments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,17 +5,18 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {Comment} from "../models/comment.model.js"
 import {Video} from "../models/video.model.js"
 import {Like} from "../models/like.model.js"
+import {User} from "../models/user.model.js"
 
 const getVideoComments = asyncHandler( async(req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page=1, limit = 10} = req.query
 
-    const video = await Video.findById(videoId)
+    const video = await Video.findById(videoId) // found the video 
 
     if(!isValidObjectId(videoId)){
         throw new ApiError(404, "invalid video id")
-    }
+    } // check if the id is valid
 
     const options = {
         page: parseInt(page),
@@ -23,7 +24,7 @@ const getVideoComments = asyncHandler( async(req, res) => {
     
     }
 
-    const comment = await Comment.aggregate([
+    const allComments = await Comment.aggregate([
         {
             $match: new mongoose.Types.ObjectId(videoId)
         },
@@ -48,7 +49,7 @@ const getVideoComments = asyncHandler( async(req, res) => {
                     {
                         $group: {
                             _id: "$liked",
-                            owners: {$push: "$likedBy"}
+                            owners: {$push: "$likedBy"} // inside likes we have user _id who like the comment
                         }
                     }
                 ]
@@ -71,15 +72,155 @@ const getVideoComments = asyncHandler( async(req, res) => {
                         $group:{
                             _id: "Liked",
                             owners: {
-                                $push: "$likedBy"
+                                $push: "$likedBy" // inside dislikes we have user _id who dislikes the comment
                             }
                         }
                     }
                 ]
             }
+        },
+        {
+            $addFields:{
+                likes: {
+                    $cond:{
+                        if:{
+                            $gt: [{$size: "$likes"},0]
+                        },
+                        then: {$first: "$likes.owners"}, // add a field "likes" which has the owner
+                        else: []
+                    }
+                },
+                dislikes:{
+                    $cond:{
+                        if:{
+                            $gt:[{$size: "$dislikes"}, 0]
+                        },
+                        then:{
+                            $first: "$dislikes.owners" // add a field "dislikes" which has the owner
+                        },
+                        else: []
+                    }
+                }
+            }
+        },
+        {
+            // get the user
+            $lookup:{
+                from: "User",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline:[
+                    {
+                        $project:{
+                            fullName: 1,
+                            userName: 1,
+                            avatar: 1,
+                            _id: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$owner" // unwind so multiple documents with different owners details
+        },
+        {
+            $project:{
+                content:1,
+                owner: 1,
+                createdAt:1,
+                updatedAt:1,
+                isOwner:{
+                    $cond:{
+                        if:{
+                            $in: ["req.user._id", "$owner._id"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                },
+                likesCount:{
+                    $size: "$likes"
+                },
+                dislikesCount:{
+                    $size: "$dislikes"
+                },
+                isLiked:{
+                    $cond:{
+                        if:{
+                            $in: [req.user._id, "$likes"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                },
+                isDisliked:{
+                    $cond:{
+                        if:{
+                            $in:[req.user._id, "$dislikes"]
+                        },
+                        then : true,
+                        else: false
+                    }
+                },
+                isLikedByOwner:{
+                    $cond:{
+                        if:{
+                            $in:[video.owner, "$likes"] // we have find the video from videoId , we use that to find the owner of the video and check it with likes
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
         }
     ])
 
+    res
+    .status(200)
+    .json(
+        new ApiResponse(200, allComments, "All comments are sent")
+    )
+
+    //now send the paginated comments
+    Comment.aggregatePaginate(allComments, options, function(error, results){
+        if(!error){
+            const{
+                docs,
+                totalDocs,
+                limit,
+                page,
+                totalPages,
+                pagingCounter,
+                hasNextPage,
+                hasPrevPage,
+                nextPage,
+                prevPage
+            } = results;
+        
+
+            return res
+            .status(200)
+            .json(new ApiResponse(200, {
+                allComments,
+                paginatedComments: docs,
+                totalDocs,
+                limit,
+                page,
+                totalPages,
+                pagingCounter,
+                hasNextPage,
+                hasPrevPage,
+                nextPage,
+                prevPage
+            },
+            "All paginated comments are sent successfully"
+        ))
+        } else{
+            throw new ApiError(408, "fault in sending paginatd comments" )
+        }
+    })
 })
 
 const addComment = asyncHandler (async(req, res) => {
